abfa0e7bf48a43f6c8e1bb819c71c8107ce8a65e
joeevins <joeevins@gmail.com>
2019-04-24 11:10:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-phutus
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-phutus)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
